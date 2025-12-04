4d85b7c35404d0fbb3a3432ac6f8e9db8cb8aec2
Elle John Cristobal <ellemenopi.qrst@gmail.com>
2025-12-04 09:23:03 +0800

    Edited Header

--- components/Header.js
+++ components/Header.js
@@ -32,7 +32,7 @@ export default function Header() {
               <span className="text-white font-bold text-xl">A</span>
             </div>
             <span className="text-2xl font-bold bg-gradient-to-r from-slate-900 to-slate-700 bg-clip-text text-transparent">
-              alfred ai
+              Alfred AI
             </span>
           </div>
           
